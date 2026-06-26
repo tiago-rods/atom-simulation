4bd4198addd768265d46b1400249308886ff9eb1
tiago-rods <t.alves.rodrigues@hotmail.com>
2026-06-26 02:02:30 -0300

    feat orbital simulation

--- src/simulation/ISimulationObserver.h
+++ src/simulation/ISimulationObserver.h
@@ -5,5 +5,7 @@
 class ISimulationObserver {
 public:
     virtual ~ISimulationObserver() = default;
-    virtual void onParticlesUpdated(const std::vector<glm::vec3>& positions) = 0;
+    virtual void onParticlesUpdated(const std::vector<glm::vec3>& positions,
+                                    const std::vector<float>& probabilities) = 0; // Separa em 2 vetores pois position vai para um VBO 
+                                    //e probabilities vai para outro VBO separado no OpenGL, dessa forma já fica no formato desejado
 };
--- src/simulation/OrbitalSimulation.cpp
+++ src/simulation/OrbitalSimulation.cpp
@@ -1,20 +1,37 @@
 #include "OrbitalSimulation.h"
+#include "core/math/SphericalCoords.h"
+#include<algorithm>
+#include <cmath>
 
-OrbitalSimulation::OrbitalSimulation(std::unique_ptr<HydrogenOrbital> orbital,
-                                     std::unique_ptr<ISampler> sampler,
-                                     int particleCount)
-    : m_orbital(std::move(orbital))
-    , m_sampler(std::move(sampler))
-    , m_count(particleCount)
+OrbitalSimulation::OrbitalSimulation(int n, int l, int m, int particleCount)
+    : m_orbital(m_factory.createOrbital(n,l,m)), m_sampler(m_factory.createSampler()), m_count(particleCount)
 {}
 
 void OrbitalSimulation::resample() {
-    auto pts = m_sampler->sample(*m_orbital, m_count);
-    notify(pts);
+    //1 . pede ao sampler N posições distribuidas conforme |Psi|^2
+    auto positions = m_sampler->sample(*m_orbital, m_count);
+    //2. para cada posição, calcula |Psi|^2 - isso da a "cor" de cada particula
+    std::vector<float> probs;
+    probs.reserve(positions.size());
+    for (const auto& p : positions){
+        auto spherical = SphericalCoords::fromCartesian(p);
+        float prob = static_cast<float>(m_orbital->psiSquared(spherical.r, spherical.theta, spherical.phi));
+        probs.push_back(prob);
+    }
+    notify(positions, probs);
 }
 
+void OrbitalSimulation::ChangeN(int delta) {
+    auto qn = m_orbital->qn();
+    int l = std::clamp(qn.n + delta, 1, MAX_N);
+    int l = std::min(qn.l, n, -1);// l < n 
+    int m = std::clamp(qn.m, -l, l); // |m| <= l
+
+
+
+}
 void OrbitalSimulation::addObserver(ISimulationObserver* obs) {
-    m_observers.push_back(obs);
+    auto qn = m_orbital->qn();
 }
 
 void OrbitalSimulation::notify(const std::vector<glm::vec3>& positions) {

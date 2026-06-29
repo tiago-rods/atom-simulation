1cfb116adf46227566cce5775f32f7fdca12c25f
tiago-rods <t.alves.rodrigues@hotmail.com>
2026-06-28 23:57:02 -0300

    chore: mudei para simular com CDFSampling, mais rápido

--- src/factory/OrbitalFactory.cpp
+++ src/factory/OrbitalFactory.cpp
@@ -1,10 +1,11 @@
 #include "OrbitalFactory.h"
 #include "core/sampling/RejectionSampler.h"
+#include "core/sampling/CDFSampler.h"
 
 std::unique_ptr<HydrogenOrbital> OrbitalFactory::createOrbital(int n, int l, int m) {
     return std::make_unique<HydrogenOrbital>(QuantumNumbers::validate(n, l, m));
 }
 
 std::unique_ptr<ISampler> OrbitalFactory::createSampler() {
-    return std::make_unique<RejectionSampler>();
+    return std::make_unique<CDFSampler>();
 }
